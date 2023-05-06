cc2e9214a4a620b588274373d4a8e37fd9fe14b8
Andrea Jota <andreacjimenez@gmail.com>
2023-05-06 10:30:27 -0400

    readme y querys base de datos

---
+++ data/data.sql
@@ -0,0 +1,142 @@
+CREATE DATABASE pezmosaico;
+
+CREATE TABLE rol(  
+    id SERIAL NOT NULL PRIMARY KEY,
+    descripcion VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE registrousuario(  
+    id SERIAL NOT NULL PRIMARY KEY,
+    nombres VARCHAR(50) NOT NULL,
+    apellidos VARCHAR(50) NOT NULL,
+    rut VARCHAR(15) NOT NULL,
+    direccion VARCHAR(50) NOT NULL,
+    telefono VARCHAR(15) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    usuario VARCHAR(50) NOT NULL,
+    password VARCHAR(150) NOT NULL,
+    rol_id INT NOT NULL
+);
+
+CREATE TABLE categoria(  
+    id SERIAL NOT NULL PRIMARY KEY,
+    categoria VARCHAR(50) NOT NULL,
+    imgCategoria VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE estado (
+  id SERIAL PRIMARY KEY,
+  descripcion varchar(50) NOT NULL
+  );
+
+CREATE TABLE producto(  
+    id SERIAL NOT NULL PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL,
+    precio INT NOT NULL,
+    imagen VARCHAR(200) NOT NULL,
+    existencia INT NOT NULL,
+    categoria_id INT NOT NULL,
+    id_estado INT NOT NULL
+);
+
+CREATE TABLE compra(  
+    id SERIAL NOT NULL PRIMARY KEY,
+    fecha VARCHAR(10) NOT NULL,
+    monto_neto INT NOT NULL,
+    id_usuario INT NULL,
+    impuesto INT NOT NULL,
+    monto_bruto INT NOT NULL,
+    gasto_envio INT NOT NULL,
+    documento_id INT NOT NULL
+);
+
+CREATE TABLE detalle_compra(  
+    id SERIAL NOT NULL PRIMARY KEY,
+    id_compra INT NOT NULL,
+    id_pdto INT NOT NULL,
+    cantidad INT NOT NULL
+);
+
+
+CREATE TABLE tipo_documento (
+  id SERIAL PRIMARY KEY,
+  nombre varchar(50) NOT NULL
+  );
+
+CREATE SEQUENCE numero_documento_seq;
+CREATE TABLE numero_documento (
+  id SERIAL PRIMARY KEY,
+    id_tipo_documento INTEGER NOT NULL REFERENCES tipo_documento(id),
+    numero INTEGER DEFAULT NEXTVAL('numero_documento_seq'),
+    UNIQUE(id_tipo_documento, numero)
+  );
+
+CREATE TABLE datos_empresa (
+  id SERIAL PRIMARY KEY,
+  nombre VARCHAR(50) NOT NULL,
+  direccion VARCHAR(100) NOT NULL,
+  telefono VARCHAR(20) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  rut VARCHAR(20) NOT NULL
+  );
+
+
+ALTER TABLE registrousuario ADD CONSTRAINT unique_usuario UNIQUE (usuario);
+
+ALTER TABLE registrousuario ADD CONSTRAINT unique_rut UNIQUE (rut);
+
+ALTER TABLE producto ADD CONSTRAINT fk_producto_estado
+  FOREIGN KEY (id_estado) REFERENCES estado (id);
+
+ALTER TABLE producto ADD CONSTRAINT fk_producto_categoria
+    FOREIGN KEY (categoria_id) REFERENCES categoria (id);
+
+ALTER TABLE registrousuario ADD CONSTRAINT fk_registrousuario_rol
+    FOREIGN KEY (rol_id) REFERENCES rol(id);
+
+ALTER TABLE compra ADD CONSTRAINT fk_compra_registrousuario
+    FOREIGN KEY (id_usuario) REFERENCES registrousuario (id);
+
+ALTER TABLE detalle_compra ADD CONSTRAINT fk_detalle_compra_compra
+    FOREIGN KEY (id_compra) REFERENCES compra (id);
+
+ALTER TABLE detalle_compra ADD CONSTRAINT fk_detalle_compra_producto
+    FOREIGN KEY (id_pdto) REFERENCES producto (id);
+
+ALTER TABLE numero_documento ADD CONSTRAINT fk_numero_documento_tipo_documento
+    FOREIGN KEY (id_tipo_documento) REFERENCES tipo_documento (id);
+
+ALTER TABLE compra ADD CONSTRAINT fk_compra_tipo_documento
+    FOREIGN KEY (documento_id) REFERENCES tipo_documento (id);
+
+
+INSERT INTO categoria (categoria, imgCategoria) VALUES 
+('Decoración Hogar', 'https://m.media-amazon.com/images/I/91NE3TwFouL.__AC_SX300_SY300_QL70_ML2_.jpg'),
+('Mobiliario', 'https://sodimac.scene7.com/is/image/SodimacCL/4160088_03?wid=1500&hei=1500&qlt=70'),
+('Misceláneo', 'https://i.pinimg.com/originals/2f/ba/f4/2fbaf4208a06cb94ca1c5c96717e54f8.jpg'),
+('Menaje Hogar', 'https://ae01.alicdn.com/kf/Scfd6410d3103440eb1a7891b62854c07N/Kit-de-posavasos-de-mosaico-redondo-de-bamb-Diy-Material-de-decoraci-n-creativo-hecho-a.jpg_Q90.jpg_.webp');
+
+
+INSERT INTO estado ("descripcion") VALUES ('disponible'), ('no disponible');
+
+INSERT INTO producto(nombre, precio, imagen, existencia, categoria_id, id_estado) VALUES 
+('Bandeja mosaico', 30000,'https://i.pinimg.com/originals/71/3d/68/713d68cce00a8937f19e524765fcb6d0.png', 10, 2, 1),
+('Cuadro mosaico', 60000, 'https://i.pinimg.com/originals/9a/0d/36/9a0d36cc7a7c75c302d7e32467243bff.jpg', 10, 1, 1),
+('Tornamesa mosaico', 45000, 'https://i.pinimg.com/originals/6a/79/23/6a7923d42f181c0f27ffbe525dcdd09d.jpg', 10, 1, 1),
+('Bandeja mosaico', 30000, 'https://i.pinimg.com/originals/79/f2/17/79f2170bb0a5ece617b9181a87f22b23.jpg', 10, 2, 1),
+('Posavasos mosaico', 10000, 'https://i.pinimg.com/originals/67/e3/e0/67e3e03865a68d2ac74aa91a73879194.jpg', 10, 2, 1),
+('Caja té mosaico', 15000, 'https://i.pinimg.com/originals/f6/78/b2/f678b2197e63bca556fd835efb45e5f5.jpg', 10, 2, 1),
+('Número casa mosaico', 25000, 'https://defrenteparaomar.com/wp-content/04-diy/201902-numero-casa-mosaico/02-numero-para-casa-mosaico.jpg', 10, 4, 1),
+('Lámpara mosaico', 45000, 'https://i.pinimg.com/originals/21/f7/b6/21f7b6539a6120d2254876adc4f1a4aa.jpg', 10, 1, 1),
+('Bandeja chica mosaico', 18000, 'https://i.pinimg.com/originals/df/4e/0e/df4e0e41d198abfa2e9141fe58066905.jpg', 10, 2, 1),
+('Cenicero mosaico', 15000, 'https://i.pinimg.com/originals/a8/1e/3f/a81e3ff7884ccd91cfbd21712a31b1fb.jpg', 10, 4, 1);
+
+INSERT INTO rol (descripcion) VALUES ('admin'), ('user');
+
+INSERT INTO registrousuario(nombres, apellidos, rut, direccion, telefono, email, usuario, password, rol_id) VALUES ('Andrea', 'Jiménez Espinoza', '9339873-4', 'Pasaje Esmeralda 14', '965554730', 'andreacjimenez@gmail.com', 'admin', '9165', 1);
+
+INSERT INTO datos_empresa (nombre, direccion, telefono, email, rut) VALUES ('Pez Mosaico Limitada', 'Pasaje Esmeralda 14, Valparaíso', '965554730', 'pezmosaico@gmail.com', '76573333-2');
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,18 @@
+                    PROYECTO PERSONAL: TIENDA PEZ MOSAICO
+
+Instrucciones para ejecutar el proyecto:
+
+1) Se deben instalar las siguientes librerías: 
+    - express: infraestructura para aplicaciones node js;
+    - pg: paquete que permite utilizar postgresSQL como base de datos.
+    - crypto-js: librería que permite encriptar contraseñas; 
+    - dotenv: paquete que permite la lectura sencilla de las variables de entorno en archivos de extensión;
+    
+
+Su instalación es su instalación se realiza en el terminal de la siguiente manera: npm i "nombre de la librería" (sin comillas) Asimismo, y para poder visualizar el proyecto en tiempo real, se sugiera instalar nodemon.
+
+2) Se incorporan las querys de entrada para cargar la base de datos con información mínima que permita visualizar el proyecto PEZ MOSAICO. Se deben ejecutar el archivo data.sql.
+
+3) La API se encuentra levantada en puerto 4000 y se accede a él por navegador en http://localhost:4000.
+
+4) Se agregaron variables de entorno, en el archivo .env ubicado en la carpeta raíz.
